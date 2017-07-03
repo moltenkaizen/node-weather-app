da1460cf740ada3efc56a75810a7ccac43f1f042
Stephen Fox <sfoxorama@gmail.com>
2017-07-03 00:18:34 -0400

    Add argument parsing with yargs and dynamically set geocoded location

--- app.js
+++ app.js
@@ -2,13 +2,28 @@
  * Created by dude on 7/2/17.
  */
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address of weather query',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+const encodedAddress = encodeURIComponent(argv.a);
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=201%20Highland%20ave%20largo%20fl',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
   console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
   console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-
 });
