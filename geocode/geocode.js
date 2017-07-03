c1aed06e98fec63be71f2b8495394f42279b41ae
Stephen Fox <sfoxorama@gmail.com>
2017-07-03 00:55:57 -0400

    Refactor geocodeAddress with callback to pass error or results object

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -3,20 +3,22 @@
  */
 const request = require('request');
 
-exports.geocodeAddress = (address) => {
+exports.geocodeAddress = (address, callback) => {
   const encodedAddress = encodeURIComponent(address);
   request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
   }, (error, response, body) => {
     if (error) {
-      console.log('Unable to connect to geocode server');
+      callback('Unable to connect to geocode server');
     } else if (body.status === 'ZERO_RESULTS') {
-      console.log('Unable to find that address');
+      callback('Unable to find that address');
     } else if (body.status === 'OK') {
-      console.log(`Address: ${body.results[0].formatted_address}`);
-      console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+      callback(undefined, {
+        address: body.results[0].formatted_address,
+        latitude: body.results[0].geometry.location.lat,
+        logitude: body.results[0].geometry.location.lng
+      });
     }
   });
 };
