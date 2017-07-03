d701c8b8b95ddf79ff7998233cd5e7858eb614a3
Stephen Fox <sfoxorama@gmail.com>
2017-07-03 00:45:34 -0400

    Refactor to abstract geocoding to separate module

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 /**
  * Created by dude on 7/2/17.
  */
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
   .options({
@@ -17,19 +17,6 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-const encodedAddress = encodeURIComponent(argv.address);
+geocode.geocodeAddress(argv.address);
 
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true
-}, (error, response, body) => {
-  if (error) {
-    console.log('Unable to connect to geocode server');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address');
-  } else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-  }
-});
+
---
+++ geocode/geocode.js
@@ -0,0 +1,22 @@
+/**
+ * Created by dude on 7/3/17.
+ */
+const request = require('request');
+
+exports.geocodeAddress = (address) => {
+  const encodedAddress = encodeURIComponent(address);
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true
+  }, (error, response, body) => {
+    if (error) {
+      console.log('Unable to connect to geocode server');
+    } else if (body.status === 'ZERO_RESULTS') {
+      console.log('Unable to find that address');
+    } else if (body.status === 'OK') {
+      console.log(`Address: ${body.results[0].formatted_address}`);
+      console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
+  });
+};
